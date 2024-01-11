0070fe19fef58ad0fb0450cfb607c96f663bf6cc
Vinit Patel <vinitpatel476@gmail.com>
2024-01-11 15:49:54 -0500

    final version

--- Assets/script.js
+++ Assets/script.js
@@ -7,76 +7,49 @@ var Temp, Wind, Humidity;
 var weatherIcon, weatherIconURL;
 var cityName;
 var cityArr = [];
-
 var TempArr = [];
 var windArr = [];
 var HumidArr = [];
 var iconArr = [];
-
+var dateArr = [];
 var totalDays = 5;
 
 //grabbing elements from the weather forecast cards.
 
 for (let i = 0; i < totalDays; i++) {
+    dateArr.push(document.querySelector(`.date_${i}`));
     TempArr.push(document.querySelector(`.temp_${i}`));
     windArr.push(document.querySelector(`.wind_${i}`));
     HumidArr.push(document.querySelector(`.humid_${i}`));
     iconArr.push(document.querySelector(`.icon_${i}`));
+    dateArr[i].textContent = dayjs().add(i + 1, `day`).format(`MM/DD/YYYY`);
     TempArr[i].textContent = `Temp : ${i} C`;
     windArr[i].textContent = `Wind : ${i} Kmph`;
     HumidArr[i].textContent = `Humidity : ${i} %`;
     iconArr[i].textContent = `${i}`;
 }
-console.log(TempArr);
 
+// to push the searched city on the localstorage and also feed the city name to api
+searchBtn.addEventListener("click", function(e) {
+    e.preventDefault();
 
-searchBtn.addEventListener("click", function(e){
-    // e.preventDefault();
-    if(cityArr.length<=0){ // prevents from resetting local storage
-        cityArr = JSON.parse(localStorage.getItem("cityArr")); 
+    if (window.localStorage.length > 0) { //avoids making cityArr null if the local storage is empty.
+        cityArr = JSON.parse(localStorage.getItem("cityArr"));
     }
-    // console.log(typeof(cityArr));
+
     cityArr.push(cityInput.value);
     localStorage.setItem("cityArr", JSON.stringify(cityArr));
-    for (let j = 0; j < cityArr.length; j++) {
-        let tempBtn = document.createElement('button');
-        tempBtn.textContent = cityArr[j];
-        tempBtn.setAttribute("class","w-100");
-        cityList.append(tempBtn);  
-        // let brEl = document.createElement(`br`);
-        // cityList.append(brEl);
-    }
-
-});
-
-
-
-
-// for (let i = 0; i < cityArr.length; i++) {
-//     localStorage.cityArr[i] = cityArr[i];
-// }
-// let forecastAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=5d4332dab2640be43629e122b795f9f2&cnt=5`;
-
-//function to capture coordinates of the city entered by the user
-
-// getAPI();
-searchBtn.addEventListener("click", function(e){
-    console.log(cityInput.value);
     cityName = cityInput.value;
     getAPI();
-    // getAPI();
 });
-
 
 // to capture user's current info using geolocation API;
 const successCallback = (position) => {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-    console.log(`lat: ${lat} lon: ${lon}`);
     let requestURL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=5d4332dab2640be43629e122b795f9f2`;
-    fetch(requestURL).then(response=>response.json()).then(data=>{
-        cityName = data[0].name;
-        console.log(cityName);
+    fetch(requestURL).then(response => response.json()).then(data => {
+        cityName = data[0].name; //fetching the user's city name based on their current coordinates.
         getAPI();
     })
 };
@@ -84,23 +57,24 @@ const successCallback = (position) => {
 const errorCallback = (error) => {
     console.log(error);
 };
+
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
-
-function getAPI(){
-    
-    
+//function to fetch weather data from the API from the city name provided.
+function getAPI() {
     let currDate = dayjs().format(`MM/DD/YYYY`);
     var cityH1 = document.querySelector(`#cityH1`);
     cityH1.textContent = `${cityName} (${currDate})`;
+
     let requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=5d4332dab2640be43629e122b795f9f2`;
-    fetch(requestURL).then(response=>response.json()).then(data=>{
+    fetch(requestURL).then(response => response.json()).then(data => {
+
+        // fetching Longtitude and Latitude from the city name user searched for and to grab it's weather data. 
         lon = parseFloat(data[0].lon);
         lat = parseFloat(data[0].lat);
 
         let requestURL1 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=5d4332dab2640be43629e122b795f9f2`
-        
-        fetch(requestURL1).then(response1=>response1.json()).then(data1=>{
+        fetch(requestURL1).then(response1 => response1.json()).then(data1 => {
             Temp = data1.main.temp;
             Humidity = data1.main.humidity;
             Wind = (data1.wind.speed * 3.5); // the result is in m/s converting it to km/h
@@ -108,21 +82,16 @@ function getAPI(){
             weatherIcon = data1.weather[0].icon;
             weatherIconURL = `https://openweathermap.org/img/w/${weatherIcon}.png`
             icon.src = weatherIconURL;
-            console.log(`Temp : ${Temp} Humidity : ${Humidity} Wind : ${Wind}`);
             displayWeatherData();
-            displayFiveDayData();
-            
+
+            // getting forecast of next 5 days
             let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=5d4332dab2640be43629e122b795f9f2&cnt=6`;
-            
-            fetch(forecastURL).then(response2=>response2.json()).then(data2=>{
+            fetch(forecastURL).then(response2 => response2.json()).then(data2 => {
                 for (let j = 0; j < totalDays; j++) {
-                    
                     TempArr[j].textContent = `Temp :${data2.list[j+1].main.temp} °C`
                     windArr[j].textContent = `Wind :${(data2.list[j+1].wind.speed * 3.5).toFixed(2)} KM/H`;
                     HumidArr[j].textContent = `Humidity : ${data2.list[j+1].main.humidity} %`;
                     iconArr[j].src = `https://openweathermap.org/img/w/${data2.list[j+1].weather[0].icon}.png`
-                    // console.log(data2.list[j+1].weather);
-
                 }
             });
 
@@ -130,21 +99,36 @@ function getAPI(){
     });
 }
 
-function displayWeatherData(){
-    // getAPI();
-    // console.log("Outside API");
-    // var cityInfo = document.querySelector(`#cityInfo`);
+function displayWeatherData() {
     let tempData = document.querySelector(`#temp`);
     tempData.textContent = `Temp : ${Temp} °C`
-    // cityInfo.append(tempData);
     let windData = document.querySelector(`#wind`);
     windData.textContent = `Wind : ${Wind} KMPH`;
-    // cityInfo.append(windData);
     let humidityData = document.querySelector(`#humidity`);
     humidityData.textContent = `Humidity : ${Humidity} %`;
-    // cityInfo.appendChild(humidityData);
+}
+displayCityList()
+
+function displayCityList() {
+
+    if (window.localStorage.length > 0) { //avoids making cityArr null if the local storage is empty.
+        cityArr = JSON.parse(localStorage.getItem("cityArr"));
+    }
+
+    let cityListBtns = document.createElement(`div`);
+    cityListBtns.setAttribute('class', 'cityListBtns');
+    for (let j = 0; j < cityArr.length; j++) {
+        let tempBtn = document.createElement('button');
+        tempBtn.textContent = cityArr[j];
+        tempBtn.setAttribute("class", "w-100");
+        cityListBtns.append(tempBtn);
+    }
+    cityList.append(cityListBtns);
 }
 
-function displayFiveDayData(){
-
-}
+//to search weather data of the searched city.
+document.querySelector(`.cityListBtns`).addEventListener("click", function(event) {
+    event.preventDefault();
+    cityName = event.target.textContent;
+    getAPI();
+})
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Weather_Dashboard
+
+## Description
+
+I have created this small website called "Weather Dashboard". It displays weather forecast of present day and following 5 days for the searched city.
+
+## Installation
+
+<a href ="https://vinitp2.github.io/Weather_Dashboard/" target="_blank"> Click Me to access the website ! </a>
+
+## Usage
+
+This website only has a homepage. 
+
+<p>
+At the page load, the website prompts the user if they want to share their location. If the user agrees,  it uses their current coordinates to fetch the weather data using weatherAPI.
+</p>
+
+<p>
+The user can then manually search for a specific city by typing in the search bar and searching for it. Once the user searched for a city, it feeds that city name to weatherAPI and at the same time it stores the city name to user's local storage so that it can be displayed underneath a search bar as a searched city.
+</p>
+
+<p>
+The users can also select from the city names listed below the search bar as searched city history. 
+</p>
+
+<h3>The following is the list of APIs that were used in this website:</h3>
+
+<ul>
+<li><a href ="https://day.js.org/en/"> Day.js</li>
+<li><a href ="https://openweathermap.org/api"> OpenWeatherAPI</li>
+
+</ul>
+
+<h3>The following is a quick demo of the software</h3>
+
+<img src = "./Assets/Weather_Dashboard_demo.gif"/>
+
+## License
+N/A
